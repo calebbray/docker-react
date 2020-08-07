5a5f9a8207fa7e5cfeedca0f840648e95a2bf24c
Caleb Bray <caleb.bray@economicmodeling.com>
2020-08-07 16:06:47 -0700

    add a counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    setTimeout(() => setCount(count + 1), 1000)
+  }, [count])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Changed It Again</p>
+        <p>Here's the count: {count}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
